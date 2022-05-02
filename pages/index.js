4a09527d0d85ec3d7b24f0ce456bb14886f0f2dc
Seungha <seungha-0709@erpper.com>
2022-05-03 06:36:38 +0900

    add meta tag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import Head from 'next/head'
 import Home from "../components/Home";
 
-const Main = props => <Home />
+const Main = props => {
+
+  return (
+    <>
+      <Head>
+        <title>Kim Seungha, Web Frontend Developer</title>
+        <meta property="og:title" content="Kim Seungha, Web Frontend Developer" key="title" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/kimseungha.jpg" />
+        <meta property="og:description" content="웹 프런트엔드 개발자 김승하입니다." />
+      </Head>
+      <Home />
+    </>
+  )
+}
 
 export default Main
