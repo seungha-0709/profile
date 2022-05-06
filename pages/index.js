6effd4fbabd49806bf1b020bb34141b8b364d49c
Seungha <seungha-0709@erpper.com>
2022-05-06 13:06:36 +0900

    modify meta tag

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ const Portfolio = props => {
         <title>Kim Seungha, Web Frontend Developer</title>
         <meta property="og:title" content="Kim Seungha, Web Frontend Developer" key="title" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/kimseungha.jpg" />
+        <meta property="og:image" content="/website.png" />
         <meta property="og:description" content="im Seungha, Web Frontend Developer" />
       </Head>
       <div ref={componentRef}>
